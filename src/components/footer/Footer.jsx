5176e6afc6574066fdfc92ac5d33cd8a5c785ae2
miko-ahmad <michoahmad99@gmail.com>
2025-03-10 15:53:58 +0700

    fixing footers

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className=' container mx-auto bg-darkTwo mb-[26px] xl:mb-[10px] rounded-b-2xl '>
+    <div className='container mx-auto bg-darkTwo mb-[26px] xl:mb-[10px] rounded-b-2xl '>
       <div className='flex justify-center items-center p-4 text-white '>
 
         <h1>Footer</h1>
